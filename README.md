79b01695cd7960477de0336ed46c5dc45327dac7
Callum <callumbradding@gmail.com>
2025-03-18 09:58:32 +1300

    Remove programtically copying due to lack of data validation

--- README.md
+++ README.md
@@ -12,6 +12,9 @@ Notes
  - Need to install poppler and add to path
  - Need to install tesseract and add to path
 
+Instructions
+ - Need to manually copy and paste the Master Spreadsheet, as I cannot do it programmitcally due to the loss of the Data Validation on the spreadsheet.
+
 GUI
  - Tkinter
  - Or PyQt5
--- imagepdf_to_excel.py
+++ imagepdf_to_excel.py
@@ -16,19 +16,19 @@ def extract_text_from_pdf(pdf_path):
         
     return text
 
-    
+        
 def process_text_to_dataframe(text, excel_path):
-    df = pd.read_excel()
+    df = pd.read_excel(excel_path, engine='openpyxl')
     
 
 def save_to_excel(df, output_file):
     df.to_excel(output_file, index=False) 
     
 
-def main(pdf_path, excel_path): # add output_path to parameters
+def main(pdf_path, excel_path, master_doc_path): # add output_path to parameters
     text = extract_text_from_pdf(pdf_path)    
     df = process_text_to_dataframe(text, excel_path)
     # save_to_excel (df, output_path)
 
 if __name__ == "__main__":
-    main("data/KNZ151 Quote.pdf", "", "data/2025 versoin 1 -MASTER SPREADSHEET - MOTUKA (003).xlsm") # add output file
+    main("data/KNZ151 Quote.pdf", "test.xlsm") # add output file
