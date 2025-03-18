c9c661f2148126fecc2e31a53f5d400bb581eead
Callum <callumbradding@gmail.com>
2025-03-19 09:41:20 +1300

    Added code to interact with macros

---
+++ macros.py
@@ -0,0 +1,20 @@
+import xlwings as xw
+
+def call_macro(marco, workbook: xw.Book):
+    #add logic to define macros here
+    workbook.save()
+
+def call_add_labour_detail_line_macro(workbook: xw.Book):
+    workbook.macro('AddLabourDetailLine')
+  
+    
+def call_add_paint_detail_line_macro(workbook: xw.Book):
+    workbook.macro('AddPaintDetailLine')
+    
+    
+def call_add_parts_detail_line_macro(workbook: xw.Book):
+    workbook.macro('AddPartsDetailLine')
+
+
+def call_add_sublet_detail_line_macro(workbook: xw.Book):
+    workbook.macro('AddSubletDetailLine')
