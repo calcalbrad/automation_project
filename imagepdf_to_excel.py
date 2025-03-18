e89803a67b0be37a5f782f1c6a0488c415fdc07c
Callum <callumbradding@gmail.com>
2025-03-19 11:22:06 +1300

    Reduced duplication

--- imagepdf_to_excel.py
+++ imagepdf_to_excel.py
@@ -1,9 +1,8 @@
 import pytesseract
-import pandas as pd
-import openpyxl
 import xlwings as xw
 
 from pdf2image import convert_from_path
+from text_to_excel import text_to_excel
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
@@ -22,15 +21,13 @@ def extract_text_from_pdf(pdf_path):
 def append_text_to_workbook(text, excel_path):    
     with xw.App(visible=True) as app:
         workbook = app.books.open(excel_path)
-        sheet = workbook.sheets[0]
-        
-        # insert text_to_excel reference here
+        text_to_excel(workbook.sheets[0], text)
+        workbook.save()
     
     
 def main(pdf_path, excel_path):
     text = extract_text_from_pdf(pdf_path)    
-    workbook = append_text_to_workbook(text, excel_path)
-    workbook.save(excel_path)
+    append_text_to_workbook(text, excel_path)
 
 if __name__ == "__main__":
-    main("data/KNZ151 Quote.pdf", "test.xlsm")
+    main("data/KNZ151 Quote.pdf", "test.xlsx")
