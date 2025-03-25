026ab82188f8f7295c9f66df67c10071ca8f3d4c
Callum <callumbradding@gmail.com>
2025-03-26 10:51:02 +1300

    removed call macro function

--- macros.py
+++ macros.py
@@ -1,9 +1,5 @@
 import xlwings as xw
 
-def call_macro(marco, workbook: xw.Book):
-    #add logic to know which macro to call here
-    workbook.save()
-
 def call_add_labour_detail_line_macro(workbook: xw.Book):
     workbook.macro('AddLabourDetailLine')
   
